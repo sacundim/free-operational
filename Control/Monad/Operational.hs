78591d5a19c6f896f7f53244b2d05b1b01cc786c
Luis Casillas <luis@casillas.org>
2013-04-06 12:55:54 -0700

    Provide 'toFree' function in Control.Monad.Operational, and refactor 'interpret' to use that.

--- Control/Monad/Operational.hs
+++ Control/Monad/Operational.hs
@@ -5,13 +5,22 @@
 -- 'FreeT' monad transformer.
 --
 -- This module is meant to be a drop-in replacement for its
--- counterpart in the @operational@ package.
+-- counterpart in the @operational@ package.  Some of the
+-- implementation choices reflect that:
+--
+-- * @'Program' instr@ and @'ProgramView' instr@ are type synonyms for
+--   @'ProgramT' instr m@ and @'ProgramViewT' instr m@, just as in
+--   @operational@.  If you don't care for that,
+--   "Control.Monad.Operational.Simple" implements them directly in
+--   terms of 'Free'.
 module Control.Monad.Operational
     ( Program
     , ProgramView
     , view
-    , interpret
     , interpretWithMonad
+
+    , toFree
+    , interpret
 
     , ProgramT(..)
     , singleton
@@ -36,6 +45,27 @@ type ProgramView instr = ProgramViewT instr Identity
 view :: Program instr a -> ProgramView instr a
 view = runIdentity . viewT
 
+-- | This function works the same way as its counterpart in the
+-- @operational@ package, using 'view' and 'ProgramView'.
+interpretWithMonad :: Monad m =>
+                      (forall x. instr x -> m x)
+                   -> Program instr a
+                   -> m a
+interpretWithMonad evalI = eval . view
+    where eval (Return a) = return a
+          eval (i :>>= k) = evalI i >>= interpretWithMonad evalI . k
+
+
+
+-- | The 'Free' monad action for a 'Program'.
+toFree :: Program instr a -> Free (Yoneda instr) a
+toFree = freeT2Free . toFreeT
+
+freeT2Free :: Functor f => FreeT f Identity a -> Free f a
+freeT2Free = adjust . runIdentity . runFreeT
+    where adjust (Pure a) = Free.Pure a
+          adjust (Free fb) = Free.Free $ fmap freeT2Free fb
+
 -- | Interpret a 'Program' by interpreting each instruction as a
 -- monadic action.  Unlike 'interpretWithMonad', this soes not use
 -- 'view' nor 'ProgramView'.
@@ -46,24 +76,11 @@ interpret :: forall m instr a. (Functor m, Monad m) =>
              (forall x. instr x -> m x)
           -> Program instr a
           -> m a
-interpret evalI = Free.retract . toFree . transFreeT evalF . toFreeT
+interpret evalI = Free.retract . Free.hoistFree evalF . toFree
     where evalF :: forall x. Yoneda instr x -> m x
           evalF (Yoneda f i) = liftM f (evalI i)
 
-toFree :: Functor f => FreeT f Identity a -> Free f a
-toFree = adjust . runIdentity . runFreeT
-    where adjust (Pure a) = Free.Pure a
-          adjust (Free fb) = Free.Free $ fmap toFree fb
 
--- | This function works the same way as its counterpart in the
--- @operational@ package, using 'view' and 'ProgramView'.
-interpretWithMonad :: Monad m =>
-                      (forall x. instr x -> m x)
-                   -> Program instr a
-                   -> m a
-interpretWithMonad evalI = eval . view
-    where eval (Return a) = return a
-          eval (i :>>= k) = evalI i >>= interpretWithMonad evalI . k
 
 newtype ProgramT instr m a = 
     ProgramT { toFreeT :: FreeT (Yoneda instr) m a }
