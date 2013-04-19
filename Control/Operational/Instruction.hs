9cdf0d37895d753c26ac2432ab8ff005f7238947
Luis Casillas <Luis.Casillas@nice.com>
2013-04-18 17:22:05 -0700

    Reexport Data.Functor.Coproduct from Control.Operational.Instruction, since it's so useful for unions of instruction sets.

--- Control/Operational/Instruction.hs
+++ Control/Operational/Instruction.hs
@@ -1,17 +1,34 @@
 {-# LANGUAGE Rank2Types #-}
 
+-- | Utility functions for working with instructions and instruction sets.
+--
+-- The "Data.Functor.Coproduct" module is very useful with instruction
+-- sets, so this module reexports it.  The 'Coproduct' type can be
+-- used to take the union of two instruction sets, and the 'coproduct'
+-- function can be used to construct an instruction evaluation for
+-- such an union.  So if we have these two instruction evaluations:
+--
+-- > evalI  :: forall x. instr  x -> f x
+-- > evalI' :: forall x. instr' x -> f x 
+--
+-- then their 'coproduct' is an evaluation for the union of the
+-- instruction sets:
+-- 
+-- > coproduct evalI evalI' :: forall x. (Coproduct instr instr' x) -> f x
 module Control.Operational.Instruction 
-    ( liftInstr
+    ( module Data.Functor.Coproduct
     , liftEvalI
+    , liftInstr
     ) where
 
+import Data.Functor.Coproduct
 import Data.Functor.Yoneda.Contravariant
-
-liftInstr :: instr a -> Yoneda instr a
-liftInstr = liftYoneda
 
 -- | Lift an operational instruction evaluator into a free 'Functor'
 -- evaluator.
 liftEvalI :: Functor f => (forall x. instr x -> f x)  -> Yoneda instr a -> f a
 liftEvalI evalI (Yoneda f i) = fmap f (evalI i) 
+
+liftInstr :: instr a -> Yoneda instr a
+liftInstr = liftYoneda
 
